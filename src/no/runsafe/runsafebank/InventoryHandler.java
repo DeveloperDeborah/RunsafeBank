bab8cb4447d6ef53fee7320fdc36fcf4246ff71b
Kruithne <kruithne@gmail.com>
2013-05-01 18:18:13 +0100

    Implemented a basic test case

---
+++ src/no/runsafe/runsafebank/InventoryHandler.java
@@ -0,0 +1,6 @@
+package no.runsafe.runsafebank;
+
+public class InventoryHandler
+{
+
+}
--- src/no/runsafe/runsafebank/events/Interact.java
+++ src/no/runsafe/runsafebank/events/Interact.java
@@ -1,21 +1,36 @@
 package no.runsafe.runsafebank.events;
 
 import no.runsafe.framework.event.player.IPlayerInteractEvent;
-import no.runsafe.framework.server.RunsafeServer;
+import no.runsafe.framework.output.IOutput;
+import no.runsafe.framework.serialization.Serializer;
 import no.runsafe.framework.server.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.framework.server.inventory.RunsafeInventory;
+import no.runsafe.framework.server.item.RunsafeItemStack;
+
+import no.runsafe.framework.server.player.RunsafePlayer;
 import org.bukkit.Material;
-import org.bukkit.event.inventory.InventoryType;
+
+import java.util.List;
+import java.util.Map;
 
 public class Interact implements IPlayerInteractEvent
 {
+	public Interact(IOutput output)
+	{
+		this.output = output;
+	}
+
 	@Override
 	public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
 	{
 		if (event.getBlock().getTypeId() == Material.ENDER_CHEST.getId())
 		{
-			RunsafeInventory newInventory = RunsafeServer.Instance.createInventory(null, 54, "Bank");
-			event.getPlayer().openInventory(newInventory);
+			RunsafePlayer player = event.getPlayer();
+			String test = player.getInventory().serialize();
+
+			output.write(test);
 		}
 	}
+
+	private IOutput output;
 }
