517d4af66871e5944d9239df87c0790e4b48640f
Kruithne <kruithne@gmail.com>
2013-05-01 18:19:40 +0100

    Removed development import

--- src/no/runsafe/runsafebank/events/Interact.java
+++ src/no/runsafe/runsafebank/events/Interact.java
@@ -2,16 +2,10 @@ package no.runsafe.runsafebank.events;
 
 import no.runsafe.framework.event.player.IPlayerInteractEvent;
 import no.runsafe.framework.output.IOutput;
-import no.runsafe.framework.serialization.Serializer;
 import no.runsafe.framework.server.event.player.RunsafePlayerInteractEvent;
-import no.runsafe.framework.server.inventory.RunsafeInventory;
-import no.runsafe.framework.server.item.RunsafeItemStack;
 
 import no.runsafe.framework.server.player.RunsafePlayer;
 import org.bukkit.Material;
-
-import java.util.List;
-import java.util.Map;
 
 public class Interact implements IPlayerInteractEvent
 {
