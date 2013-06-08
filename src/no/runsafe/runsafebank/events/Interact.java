84756e948ac1cef8b4705a1b05b99bba994b2f86
Morten Nilsen <morten@runsafe.no>
2013-06-08 23:24:52 +0200

    Disuse deprecated method

--- src/no/runsafe/runsafebank/events/Interact.java
+++ src/no/runsafe/runsafebank/events/Interact.java
@@ -1,13 +1,11 @@
 package no.runsafe.runsafebank.events;
 
 import no.runsafe.framework.event.player.IPlayerInteractEvent;
+import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.event.player.RunsafePlayerInteractEvent;
-
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.runsafebank.BankHandler;
-import org.bukkit.Material;
-import org.bukkit.event.block.Action;
 
 public class Interact implements IPlayerInteractEvent
 {
@@ -20,18 +18,15 @@ public class Interact implements IPlayerInteractEvent
 	public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
 	{
 		RunsafeBlock block = event.getBlock();
-		if (block != null && event.getAction() == Action.RIGHT_CLICK_BLOCK)
+		if (block != null && event.isRightClick() && block.is(Item.Decoration.EnderChest))
 		{
-			if (block.getTypeId() == Material.ENDER_CHEST.getId())
-			{
-				RunsafePlayer player = event.getPlayer();
-				if (player.hasPermission("runsafe.bank.use"))
-					this.bankHandler.openBank(player, player);
-				else
-					player.sendColouredMessage("&cYou do not have permissions to use the bank.");
+			RunsafePlayer player = event.getPlayer();
+			if (player.hasPermission("runsafe.bank.use"))
+				this.bankHandler.openBank(player, player);
+			else
+				player.sendColouredMessage("&cYou do not have permissions to use the bank.");
 
-				event.setCancelled(true);
-			}
+			event.setCancelled(true);
 		}
 	}
 
