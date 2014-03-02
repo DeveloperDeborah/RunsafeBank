9bb5db4e763f51ad93a75d31a5a354ea27ace402
Morten Nilsen <morten@runsafe.no>
2014-03-02 21:48:21 +0100

    API usage change

--- src/no/runsafe/runsafebank/commands/ViewBank.java
+++ src/no/runsafe/runsafebank/commands/ViewBank.java
@@ -1,37 +1,30 @@
 package no.runsafe.runsafebank.commands;
 
-import no.runsafe.framework.api.IServer;
-import no.runsafe.framework.api.command.argument.AnyPlayerRequired;
 import no.runsafe.framework.api.command.argument.IArgumentList;
+import no.runsafe.framework.api.command.argument.Player;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.runsafebank.BankHandler;
 
 public class ViewBank extends PlayerCommand
 {
-	public ViewBank(BankHandler bankHandler, IServer server)
+	public ViewBank(BankHandler bankHandler)
 	{
 		super(
 			"viewbank", "Opens a players bank", "runsafe.bank.view",
-			new AnyPlayerRequired()
+			new Player.Any().require()
 		);
 		this.bankHandler = bankHandler;
-		this.server = server;
 	}
 
 	@Override
 	public String OnExecute(IPlayer executor, IArgumentList parameters)
 	{
-		IPlayer player = server.getPlayer(parameters.get("player"));
-
+		IPlayer player = parameters.getValue("player");
 		if (player != null)
 			this.bankHandler.openBank(executor, player);
-		else
-			executor.sendColouredMessage("&cThe player you are looking for does not exist.");
-
 		return null;
 	}
 
 	private BankHandler bankHandler;
-	private final IServer server;
 }
