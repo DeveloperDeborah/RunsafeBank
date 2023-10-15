880871977974e0383a1f666a36c5b4c18629dd44
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-10-15 18:29:57 -0400

    Add whitelist for worlds the bank can be used in

--- src/no/runsafe/runsafebank/events/Interact.java
+++ src/no/runsafe/runsafebank/events/Interact.java
@@ -1,13 +1,18 @@
 package no.runsafe.runsafebank.events;
 
+import no.runsafe.framework.api.IConfiguration;
 import no.runsafe.framework.api.block.IBlock;
 import no.runsafe.framework.api.event.player.IPlayerInteractEvent;
+import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerInteractEvent;
 import no.runsafe.runsafebank.BankHandler;
 
-public class Interact implements IPlayerInteractEvent
+import java.util.ArrayList;
+import java.util.List;
+
+public class Interact implements IPlayerInteractEvent, IConfigurationChanged
 {
 	public Interact(BankHandler bankHandler)
 	{
@@ -15,20 +20,30 @@ public class Interact implements IPlayerInteractEvent
 	}
 
 	@Override
+	public void OnConfigurationChanged(IConfiguration config)
+	{
+		worlds.clear();
+		worlds.addAll(config.getConfigValueAsList("worlds"));
+	}
+
+	@Override
 	public void OnPlayerInteractEvent(RunsafePlayerInteractEvent event)
 	{
 		IBlock block = event.getBlock();
-		if (block != null && event.isRightClick() && block.is(Item.Decoration.EnderChest))
-		{
-			IPlayer player = event.getPlayer();
-			if (player.hasPermission("runsafe.bank.use"))
-				this.bankHandler.openBank(player, player);
-			else
-				player.sendColouredMessage("&cYou do not have permissions to use the bank.");
+		if (block == null || !event.isRightClick() || !block.is(Item.Decoration.EnderChest))
+			return;
 
-			event.cancel();
-		}
+		IPlayer player = event.getPlayer();
+
+		if (!player.hasPermission("runsafe.bank.use"))
+			player.sendColouredMessage("&cYou do not have permissions to use the bank.");
+		else if (!worlds.contains(player.getWorldName()))
+			player.sendColouredMessage("&cYou can not use the bank in this world.");
+		else this.bankHandler.openBank(player, player);
+
+		event.cancel();
 	}
 
 	private final BankHandler bankHandler;
+	private final List<String> worlds = new ArrayList<>(0);
 }
