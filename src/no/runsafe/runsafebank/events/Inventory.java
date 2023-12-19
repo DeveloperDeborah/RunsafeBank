26be39157e6fb54f0d1df9fdaef6fb9a9f52e935
DevDeborah <softwaredeveloperdeborah@gmail.com>
2023-12-19 17:06:09 -0500

    Fix using wrong event to check for blacklisted items

--- src/no/runsafe/runsafebank/events/Inventory.java
+++ src/no/runsafe/runsafebank/events/Inventory.java
@@ -1,16 +1,16 @@
 package no.runsafe.runsafebank.events;
 
 import no.runsafe.framework.api.ILocation;
-import no.runsafe.framework.api.event.inventory.IInventoryMoveItem;
+import no.runsafe.framework.api.event.inventory.IInventoryClick;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.Sound;
-import no.runsafe.framework.minecraft.event.inventory.RunsafeInventoryMoveItemEvent;
+import no.runsafe.framework.minecraft.event.inventory.RunsafeInventoryClickEvent;
 import no.runsafe.runsafebank.BankHandler;
 import no.runsafe.runsafebank.Config;
 
 import java.util.List;
 
-public class Inventory implements IInventoryMoveItem
+public class Inventory implements IInventoryClick
 {
 	public Inventory(BankHandler bankHandler)
 	{
@@ -18,16 +18,16 @@ public class Inventory implements IInventoryMoveItem
 	}
 
 	@Override
-	public void OnInventoryMoveItemEvent(RunsafeInventoryMoveItemEvent event)
+	public void OnInventoryClickEvent(RunsafeInventoryClickEvent event)
 	{
-		if (!event.getDestination().getName().contains("'s Bank Vault"))
+		if (!event.getInventory().getName().contains("'s Bank Vault"))
 			return;
 
-		List<IPlayer> viewers = event.getDestination().getViewers();
+		List<IPlayer> viewers = event.getInventory().getViewers();
 		if (!bankHandler.isViewingBank(viewers)) // Double check someone didn't name a random chest "'s Bank Vault"
 			return;
 
-		if (!Config.isBlacklistedItem(event.getItem()))
+		if (!Config.isBlacklistedItem(event.getCurrentItem()))
 			return;
 
 		event.cancel();
